47a28e0670a4e3c00d81ad70cacbaaeb1dff74fe
Kipras Melnikovas <kipras@kipras.org>
2021-05-29 15:49:44 +0300

    update the `me` query's cache after `register`/`login` mutations
    without this, once you register/login, the parts of the app that used
    the `me` query would have the cached result which was previously `null`
    and so it'd be out of sync.
    
    We could invalidate the cache instead - it would make an extra request tho
    and here we already have all the data we need.
    
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,59 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { createClient, Provider as UrqlProvider } from "urql";
-
+import { createClient, dedupExchange, fetchExchange, Provider as UrqlProvider } from "urql";
+import { cacheExchange } from "@urql/exchange-graphcache";
 import { AppProps } from "next/app";
+
 import theme from "../theme";
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from "../generated/graphql";
 
 const urqlClient = createClient({
 	url: "http://localhost:5000/graphql",
 	fetchOptions: {
 		credentials: "include",
 	},
+	exchanges: [
+		dedupExchange, //
+		cacheExchange({
+			updates: {
+				Mutation: {
+					/**
+					 * update the `me` query's cache after `register`/`login` mutations
+					 *
+					 * without this, once you register/login, the parts of the app that used
+					 * the `me` query would have the cached result which was previously `null`
+					 * and so it'd be out of sync.
+					 *
+					 * We could invalidate the cache instead - it would make an extra request tho
+					 * and here we already have all the data we need.
+					 *
+					 */
+					loginUser: (result: LoginMutation, _args, cache, _info): void => {
+						cache.updateQuery({ query: MeDocument }, (query: MeQuery | null) => {
+							if (!result.loginUser) {
+								return query;
+							} else {
+								return {
+									me: result.loginUser,
+								};
+							}
+						});
+					},
+					registerUser: (result: RegisterMutation, _args, cache, _info): void => {
+						cache.updateQuery({ query: MeDocument }, (query: MeQuery | null) => {
+							if (!result.registerUser) {
+								return query;
+							} else {
+								return {
+									me: result.registerUser,
+								};
+							}
+						});
+					},
+				},
+			},
+		}),
+		fetchExchange,
+	],
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
